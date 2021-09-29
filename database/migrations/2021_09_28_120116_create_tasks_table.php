fe06f754ae79ccd27c315f38d09aa4abf748de1e
Rahmafatin Nurul Izza <rhmftn.ni@gmail.com>
2021-09-29 14:30:37 +0700

    Cread Read Task done

--- database/migrations/2021_09_28_120116_create_tasks_table.php
+++ database/migrations/2021_09_28_120116_create_tasks_table.php
@@ -16,8 +16,8 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('task_name');
-            $table->string('task_description');
-            $table->string('status');
+            $table->string('task_description')->nullable();
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
