32564fa407e1d0bf0c8aadb6095cf0e0ed7da6cb
diazabdillah20 <diazabdillah20@gmail.com>
2021-11-04 14:30:03 +0700

    crud report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
+    public function index(){
+        $report = DB::table('users')
+        ->leftJoin('workspace', 'users.id', '=', 'workspace.id')
+        ->leftJoin('task', 'users.id', '=', 'task.id')
+        ->leftJoin('balance', 'users.id', '=', 'balance.id')
+       ->select('workspace.nama','task.nama','balance.incomme','tbalance.outcome')
+        ->where('users.id', '=', $id)
+        ->get();
 
+    return response()->json($report, 200);
+
+
+    }
 }
