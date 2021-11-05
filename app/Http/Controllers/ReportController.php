da87b2b30aa90c8f57943e94ce2754bbb59ce687
Rahmafatin Nurul Izza <rhmftn.ni@gmail.com>
2021-11-06 00:52:13 +0700

    Create Report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Models\Report;
+use App\Models\User;
+use App\Models\Balance;
+use App\Models\Workspace;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -16,6 +21,38 @@ class ReportController extends Controller
         ->get();
 
       return response()->json($report, 200);
-
     }
+
+  public function create(int $workspace_id){
+    //mengambil user yang sedang aktif
+    $user = Auth::user();
+
+    //apakah workspace nya ada 
+    $workspace = Workspace::firstWhere('id', $workspace_id);
+    if(!$workspace){
+      return response()->json([
+        'message' => 'workspace unavailable'
+      ],404);
+    }
+
+    //kalau workspace nya ada cek user access nya ke workspace tersebut
+    $this->userAccess($user,$workspace);
+
+    //user punya access maka create report
+    $report_name = $workspace->workspace_name .' report';
+
+    $report = new Report([
+      'workspace_id' => $workspace->id,
+      'report_name' => $report_name
+    ]);
+    $report->save();
+  }
+
+  public function userAccess(User $user, Workspace $workspace){
+    
+    if(!($user->workspaces()->where('workspace-id', $workspace->id)))
+        return response()->json([
+          'message' => 'Access denied'
+        ],403);
+  }
 }
